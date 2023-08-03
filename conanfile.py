a51b2d813f53c75120f113ce01d01b57da2e352f
jona <jonathanmalinza@gmail.com>
2023-08-03 18:17:45 +0900

    Add generate and build methods to conan recipe

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,8 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import cmake_layout, CMakeDeps, CMakeToolchain, CMake
 
-class CompressorRecipe(ConanFile):
+class StarterRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
 
     def requirements(self):
         self.requires("boost/1.80.0")
@@ -13,3 +12,15 @@ class CompressorRecipe(ConanFile):
     
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self, generator="Ninja")
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake_variables = {}
+        cmake.configure(cmake_variables)
+        cmake.build()
